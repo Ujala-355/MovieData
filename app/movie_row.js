61176f4df5affd5229e0a29cd3c90b37a11af5da
Ujala <ujala21@navgurukul.com>
2023-07-18 12:58:31 +0530

    Like Dislike button done

--- app/movie_row.js
+++ app/movie_row.js
@@ -1,30 +1,32 @@
 "use client"
 import DeleteButton from './delete';
 import Rating from "./rating"
-function Movie_row({movie,onDelete,handleLike,handleDislike}){
-  return(
+
+function Movie_row({ movie, onDelete, handleLike, handleDislike, voteCount }) {
+  return (
     <>
-        <div className="row4">
-            <div className="col row4_col1">
-                    <img className="poster-path" src={movie.poster_path ? `http://image.tmdb.org/t/p/w500${movie.poster_path}` : ''}/>
-            </div>
-                <div className="col row4_col2">
-                        <p className="Text col2text1">{movie.title}</p>
-                        <span className="Text col2text2">{movie.relemoviedataase_date}</span>
-                        <h3>Description</h3>
-                        <div className="col2text">
-                            <p className="col2text3">{movie.overview}</p>
-                        </div>
-                        <>
-                          <DeleteButton onDelete={onDelete}/> 
-                          <Rating  handleLike={handleLike} handleDislike={handleDislike} />
-                        </>
-                </div>
+      <div className="row4">
+        <div className="col row4_col1">
+          <img className="poster-path" src={movie.poster_path ? `http://image.tmdb.org/t/p/w500${movie.poster_path}` : ''} />
         </div>
-        <div className="line"></div>
-     </>
+        <div className="col row4_col2">
+          <p className="Text col2text1">{movie.title}</p>
+          <span className="Text col2text2">{movie.releasedate}</span>
+          <h3>Description</h3>
+          <div className="col2text">
+            <p className="col2text3">{movie.overview}</p>
+          </div>
+           <>
+            <DeleteButton onDelete={onDelete} />
+            <Rating handleLike={handleLike} handleDislike={handleDislike} voteCount={voteCount} />
+          </>
+        </div>
+      </div>
+      <div className="line"></div>
+    </>
   )
 }
+
 export default Movie_row;
 
 
--- app/page.js
+++ app/page.js
@@ -1,57 +1,77 @@
 "use client"
 import Header from "./header";
 import Footer from "./footer";
-import Movie_row from './movie_row'; 
+import Movie_row from './movie_row';
 
-import React, {useState, useEffect} from 'react';
-const Page=()=>{
-    const [data, setData]=useState([]);
-    const [voteingCount, setVoteingCount]=useState(0);
-    const handleLike=()=>{
-            setVoteingCount(voteingCount+1);
-        };
-        const handleDislike=()=>{
-            setVoteingCount(voteingCount-1);
-        };
-        
-    useEffect(() => {
+import React, { useState, useEffect } from 'react';
+const Page = () => {
+  const [data, setData] = useState([]);
+  const [voteCount, setVoteCount] = useState({});
+  const handleLike=(id)=>{
+    const Votes={...voteCount}
+    if (Votes[id]===undefined){
+        Votes[id]=1
+     }
+     else {
+        Votes[id]+=1
+     }
+      setVoteCount(Votes);
+   };
+  const handleDislike=(id)=>{
+      const Votes={...voteCount}
+      if (Votes[id]===undefined){
+        Votes[id]=-1
+     }
+     else {
+        Votes[id]-=1
+     }
+      setVoteCount(Votes);
+  };
+  useEffect(() => {
     async function fetchData(){
-        try{
-            const response=await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357');
-            const data=await response.json();
-            const topFiveMovies=data.results.slice(5, 15);
-            setData(topFiveMovies);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357');
+        const data = await response.json();
+        const topFiveMovies = data.results.slice(5, 15);
+        setData(topFiveMovies);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     }
     fetchData();
-    },[]);
-    const handleDeleteMovie = (movieId) => {
-        setData((prevData) => prevData.filter((movie) => movie.id !== movieId));
-    };
-    
-    return (
-        <div>
-            <Header />
-                {data.length > 0 ? (
-                    data.map((movie) => (
-                        <div key={movie.id}>
-                        <Movie_row
-                            movie={movie}
-                            onDelete={() => handleDeleteMovie(movie.id)}
-                            handleLike={()=>}
-                            handleDislike
-                            
-                        />
-                        </div>
-                    ))
-                ) : (
-                    <p>Loading...</p>
-                )}
-            <Footer />
-        </div>
-    );
+  }, []);
+
+  const handleDeleteMovie = (movieId) => {
+    setData((prevData) => prevData.filter((movie) => movie.id !== movieId));
+  };
+
+  const sortVotes=data.sort((a,b) => {
+    const vote1=voteCount[a.id]||0;
+    const vote2=voteCount[b.id]||0;
+    return vote2 - vote1 ;
+  });
+  return (
+    <div>
+      <Header />
+      {data.length > 0 ? (
+        data.map((movie) => (
+          <div key={movie.id}>
+            <Movie_row
+              movie={movie}
+              onDelete={() => handleDeleteMovie(movie.id)}
+              handleLike={() => handleLike(movie.id)}
+              handleDislike={() => handleDislike(movie.id)}
+              voteCount={voteCount[movie.id]}
+              
+            />
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Footer />
+    </div>
+  );
 };
 
 export default Page;
